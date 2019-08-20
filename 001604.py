77040e93f7c2bb4ea83cfc6688d2a872d54c715d
matias-real <53578787+matias-real@users.noreply.github.com>
2019-08-19 22:41:26 -0400

    Add files via upload

---
+++ 001604.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 19 21:31:19 2019
+
+@author: USUARIO
+"""
+
+g = 19 # con esto le estoy asignando a la variable de la izquierda el valor de la derecha
+h = 19 # con esto le estoy asignando a la variable de la izquierda el valor de la derecha
+if g < h: # Con esto digo: si g < h que pase lo que este identado dentro del if
+    print "g es menor que h"
+
+else: #esta parte del codigo sirve para no utilizar elif 
+    if g  == h:
+        print "g es igual a h" #esto se imprime solo si g es igual a h
+    else:
+        print "g es mas grande que h" #esto se imprime solo si g  es mas grande que h
